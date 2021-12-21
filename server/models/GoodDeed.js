cf3fbfc43548f52e08e24220e722914f23c2e8a8
Michelle Wehr <michelle_wehr@icloud.com>
2021-12-21 12:44:27 -0500

    add the required commentschema to file

--- server/models/GoodDeed.js
+++ server/models/GoodDeed.js
@@ -1,5 +1,6 @@
 const { Schema, model } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
+const commentSchema = require('./Comment');
 
 const goodDeedSchema = new Schema(
     {
