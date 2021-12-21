b683ad4a560c587409cf7f4184567319d05eface
Michelle Wehr <michelle_wehr@icloud.com>
2021-12-21 12:42:54 -0500

    add gooddeed model

---
+++ server/models/GoodDeed.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const goodDeedSchema = new Schema(
+    {
+        host: {
+            type: String, 
+            required: true,
+            trim: true
+        },
+        title: {
+            type: String, 
+            required: true, 
+            trim: true
+        },
+        deedText: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: timestamp => dateFormat(timestamp)
+          },
+        comments: [commentSchema],
+        likes: {
+            type: Int,
+            default: 0
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+)
+
+const GoodDeed = model('GoodDeed',goodDeedSchema);
+
+//TODO: view count/ comment count?
+
+module.exports = GoodDeed;
