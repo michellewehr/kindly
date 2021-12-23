c94a2b2291fabe7b1cb503a830aa675e3e6cc5b1
DaveToth77 <85111342+DaveToth77@users.noreply.github.com>
2021-12-23 11:23:30 -0500

    added gooddeed page

---
+++ client/src/pages/GoodDeeds.js
@@ -0,0 +1,15 @@
+import Header from "../components/Header";
+import SideBar from "../components/SideBar";
+import GoodDeed from "../components/GoodDeed";
+import Footer from "../components/Footer";
+
+export default function GoodDeeds() {
+  return (
+    <div>
+      <Header />
+      <SideBar />
+      <GoodDeed />
+      <Footer />
+    </div>
+  )
+}
