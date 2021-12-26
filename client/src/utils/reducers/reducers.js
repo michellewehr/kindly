e6eebc8a3b5da20bc64f52f899e17ad73ac26fc8
DaveToth77 <85111342+DaveToth77@users.noreply.github.com>
2021-12-26 14:26:04 -0500

    added: redux files
    added: auth.js-JWT decoding

---
+++ client/src/utils/reducers/reducers.js
@@ -0,0 +1,43 @@
+import {
+  USER_SIGNUP,
+  USER_LOGIN,
+  USER_LOGOUT,
+  ADD_EVENT,
+  REMOVE_EVENT,
+  JOIN_EVENT,
+  LEAVE_EVENT,
+  VERIFY_EVENT,
+
+} from "../../constants/userConstants";
+
+// export const userLoginReducer = (state = {}, action) => {
+//   switch (action.type) {
+//     case USER_LOGIN:
+//       return { loading: true, user: action.payload };
+//     case USER_LOGOUT:
+//       return {};
+//     default:
+//       return state;
+//   }
+// };
+// export const userSignupReducer = (state = {}, action) => {
+//   switch (action.type) {
+//     case USER_SIGNUP:
+//       return { loading: true, user: action.payload };
+//     case USER_LOGOUT:
+//       return {};
+//     default:
+//       return state;
+//   }
+// };
+
+export const addEventReducer = (state = {}, action) => {
+  switch (action.type) {
+    case ADD_EVENT:
+    
+  }
+
+
+
+
+
