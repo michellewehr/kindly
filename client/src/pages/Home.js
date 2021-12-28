3bfa930d3e94334b6598938f88470750ea177fcd
Michelle Wehr <michelle_wehr@icloud.com>
2021-12-28 11:46:55 -0500

    add conditional sidebar for home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,13 +2,16 @@ import EventCard from "../components/EventCard";
 import Footer from "../components/Footer";
 // import Header from "../components/Header";
 import Sidebar from "../components/Sidebar";
+import Auth from '../utils/auth';
 
 export default function Home() {
   return (
     <div>
       {/* <Header /> */}
       <div className="flex w-full justify-between">
+        {Auth.loggedIn() && (
         <Sidebar />
+        )}
         <EventCard />
       </div>
       <Footer />
