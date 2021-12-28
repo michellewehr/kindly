dc819a758e6b5515c1c3c7513e6123f44fd26a1d
Michelle Wehr <michelle_wehr@icloud.com>
2021-12-28 14:37:32 -0500

    changed app.js back

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,18 +1,19 @@
-import EventCard from "../components/EventCard";
+import EventList from "../components/EventList";
 import Footer from "../components/Footer";
 // import Header from "../components/Header";
 import Sidebar from "../components/Sidebar";
 import Auth from '../utils/auth';
 
+
 export default function Home() {
   return (
-    <div className="min-h-screen">
+    <div>
       {/* <Header /> */}
       <div className="flex w-full justify-between">
         {Auth.loggedIn() && (
-          <Sidebar />
+        <Sidebar />
         )}
-        <EventCard />
+        <EventList />
       </div>
     </div>
   );
