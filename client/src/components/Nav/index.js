36ecc3b118d02638333425b2363f25709c61abde
Michelle Wehr <michelle_wehr@icloud.com>
2021-12-28 11:32:46 -0500

    add logout functionality and conditional nav

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -5,6 +5,10 @@ import logo from "../../assets/images/logo.png";
 export default function Nav() {
   // function showNavigation() {
   //   if (Auth.loggedIn()) {
+    const logout = event => {
+      event.preventDefault();
+      Auth.logout();
+    }
 
   return (
     <div>
@@ -21,6 +25,41 @@ export default function Nav() {
             <h1>Kindly</h1>
           </span>
         </div>
+        {Auth.loggedIn() && (
+          <>
+           <div className="block lg:hidden">
+          <button className="flex items-center px-3 py-2 border rounded text-orange-400 border-orange-400 hover:text-slate-400 hover:border-white">
+            <svg
+              className="fill-current h-3 w-3"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <title>Menu</title>
+              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+            </svg>
+          </button>
+        </div>
+        <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+          <div className="text-sm lg:flex-grow">
+            <a
+              href="#responsive-header"
+              className="block mt-4 lg:inline-block lg:mt-0 hover:text-slate-400 mr-4 text-sky-900 text-xl"
+              >          
+              My Profile
+            </a>
+            <a
+              href="#responsive-header"
+              className="block mt-4 lg:inline-block lg:mt-0 hover:text-slate-400 mr-4 text-sky-900 text-xl"
+              onClick={logout}
+            >
+              Log Out
+            </a>
+          </div>
+        </div>
+          </>
+        )}
+        {!Auth.loggedIn() && (
+          <>
         <div className="block lg:hidden">
           <button className="flex items-center px-3 py-2 border rounded text-orange-400 border-orange-400 hover:text-slate-400 hover:border-white">
             <svg
@@ -49,6 +88,8 @@ export default function Nav() {
             </a>
           </div>
         </div>
+        </>
+          )}
       </nav>
     </div>
   );
