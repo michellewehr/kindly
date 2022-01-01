8718b6dfc6893ad8cffa2838e8d0f9e885168bc4
Michelle Wehr <michelle_wehr@icloud.com>
2021-12-31 21:46:32 -0500

    conditional for only logged in users to see the number of likes/ be first to like message

--- client/src/components/EventCard/index.js
+++ client/src/components/EventCard/index.js
@@ -176,7 +176,7 @@ export default function EventCard({ event, me }) {
                 <path d="M2 10.5a1.5 1.5 0 113 0v6a1.5 1.5 0 01-3 0v-6zM6 10.333v5.43a2 2 0 001.106 1.79l.05.025A4 4 0 008.943 18h5.416a2 2 0 001.962-1.608l1.2-6A2 2 0 0015.56 8H12V4a2 2 0 00-2-2 1 1 0 00-1 1v.667a4 4 0 01-.8 2.4L6.8 7.933a4 4 0 00-.8 2.4z" />
                 </svg>
               </span>}
-              <span className="text-cyan-800">{checkLikesCount(event.likes, 'event')}</span>
+              {Auth.loggedIn() && <span className="text-cyan-800">{checkLikesCount(event.likes, 'event')}</span> }
                       </div>
         <div className="flex flex-row flex-wrap w-full px-3 md:w-2/3">
           <div className="relative w-full pt-3 font-semibold text-left text-gray-700 md:pt-0">
