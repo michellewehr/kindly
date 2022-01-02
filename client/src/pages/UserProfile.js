677eb0abaf37e095e5ed86dd1718b4c61718be6c
Michelle Wehr <michelle_wehr@icloud.com>
2022-01-01 23:43:03 -0500

    user profile functionality works

--- client/src/pages/UserProfile.js
+++ client/src/pages/UserProfile.js
@@ -1,7 +1,6 @@
 import { useQuery } from "@apollo/client";
 import { QUERY_USER } from "../utils/queries";
-import { useParams } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
 export default function UserProfile(props) {
   const { id } = useParams();
@@ -11,9 +10,8 @@ export default function UserProfile(props) {
   });
   // console.log(data, 'user data')
   const user = data?.user || {};
-  const userEvents = user.events;
-  console.log(userEvents, "user events");
-  // const userGoodDeeds = user.goodDeeds;
+  //   console.log(userEvents, "user events");
+  const userGoodDeeds = user.goodDeeds;
   // console.log(userGoodDeeds, 'good deeds user')
   // console.log(user, 'user data')
   return (
@@ -21,8 +19,8 @@ export default function UserProfile(props) {
       <div className="mx-auto">
         <div className="flex flex-col bg-white shadow-xl"></div>
         {/* Main */}
-        <div className="divide-y divide-gray-200">
-          <div className="flex items-center mx-auto">
+        <div className="">
+          <div className="flex items-center mx-auto justify-around	">
             <div className="w-1/6 mx-auto">
               <img
                 className="antialiased rounded-lg shadow-lg"
@@ -30,14 +28,14 @@ export default function UserProfile(props) {
               />
             </div>
 
-            <div className="mt-3 flex-col ">
-              <h3 className="font-bold text-4xl text-gray-900 sm:text-4xl mb-3 text-center">
-                User Name
+            <div className="mt-3 flex-col">
+              <h3 className="font-bold text-4xl text-gray-900 sm:text-4xl mb-3">
+                {user.firstName} {user.lastName}
               </h3>
 
               <div className="">
                 <p className="px-6 mx-auto mb-2 bg-orange-300 rounded w-max">
-                  Location
+                  {user.location}
                 </p>
               </div>
 
@@ -51,27 +49,28 @@ export default function UserProfile(props) {
                   <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
                 </svg>
                 <p className=" text-gray-900 ml-1">
-                  Kindly Score: Earned Kindly Points
+                  Kindly Score: {user.kindlyScore}
                 </p>
               </div>
             </div>
           </div>
         </div>
-        <div className="">
-          <div className="w-9/12 mx-auto mb-2 rounded bg-sky-100">
-            <div className="flex">
-              {/* event card */}
-              <div className="w-1/2 m-2 flex-col">
-                <h2>Upcoming Events</h2>
-                {/* for each event */}
-                {user.events.map((event) => {
+        <div className="w-11/12 mx-auto mb-2 rounded bg-sky-100">
+          {/* beginining of event/good deed section*/}
+          <div className="flex">
+            {/* events */}
+            <div className="w-1/2 m-2 flex-col">
+              <h2>Events (completed or upcoming)</h2>
+              {/* for each event */}
+              {user.events &&
+                user.events.map((event) => (
                   <div
                     key={event._id}
                     className="flex-col p-3 mt-2 antialiased bg-white rounded-lg shadow-lg  relative"
                   >
                     <div className="flex-row">
                       <div className="pb-1 text-2xl text-amber-500">
-                        <span>Event.title</span>
+                        <span>{event.title}</span>
                         {event.verifyNumber >= event.attendees.length && (
                           <div className="inline-block group">
                             <svg
@@ -93,27 +92,38 @@ export default function UserProfile(props) {
                         )}
                       </div>
 
-                      <div className="absolute right-0 top-0 m-1">
+                      <div className="absolute right-0 top-0 m-1 text-sm">
                         Kindly Points: 10
                       </div>
                     </div>
-                    <div className="pb-4 cursor-pointer text-normal hover:text-cyan-700 text-cyan-900">
-                      <Link to="">
+                    <div className=" cursor-pointer text-normal hover:text-cyan-700 text-cyan-900">
+                      <Link
+                        to={`/profile/${event.host._id}`}
+                        style={{ fontWeight: 700 }}
+                      >
                         {event.host.firstName} {event.host.lastName}
                       </Link>
                     </div>
                     <div className="flex">
-                      <p className="w-1/2">Description: {event.desciption}</p>
-                      <p className="w-1/2">Location: {event.location}</p>
-                    </div>
-                    <div className="flex">
-                      <p className="w-1/2">Date: {event.date}</p>
-                      <p className="w-1/2">
-                        Time: {event.startTime} - {event.endTime}
+                      <p className="w-2/3">
+                        <b>Description: </b> {event.desciption}
+                      </p>
+                      <p className="w-1/3">
+                        <b>Location: </b> {event.location}
                       </p>
                     </div>
                     <div className="flex">
-                      <div className=" w-1/2 hover:text-orange-300">
+                      <p className="w-2/3">
+                        <b>Date: </b>
+                        {event.date}
+                      </p>
+                      <p className="w-1/3">
+                        <b>Time: </b>
+                        {event.startTime} - {event.endTime}
+                      </p>
+                    </div>
+                    <div className="flex">
+                      <div className=" w-2/3 hover:text-orange-300">
                         <a href={event.url}>
                           <span className="w-1/2">
                             <i>Event Website</i>
@@ -123,48 +133,62 @@ export default function UserProfile(props) {
                       {/* hover to see attendees list */}
                       <div className="relative flex flex-col group w-max">
                         <span className="cursor-pointer">
-                          {event.attendees.length - 1} Attendees joining you!
+                          {event.attendees.length - 1} kind attendees
                         </span>
                       </div>
                     </div>
                     {/* button depending on attendence to join/leave/cancel event*/}
-                  </div>;
-                })}
+                  </div>
+                ))}
 
-                {/* end of for each event */}
-              </div>
-              {/* good deeds */}
-              <div className="w-1/2 m-2 flex-col">
-                <h2>Upcoming Good Deeds</h2>
-                <div className="flex-col p-3 mt-2 antialiased bg-white rounded-lg shadow-lg  relative">
-                  <div className="flex-row">
-                    <div className="pb-1 text-2xl text-amber-500">
-                      <span>Good Deed Title</span>
-                    </div>
-                    <div className="absolute right-0 top-0 m-1">
-                      Kindly Points: 10
-                    </div>
-                  </div>
-                  <div className="pb-4 cursor-pointer text-normal hover:text-cyan-700 text-cyan-900">
-                    <Link to="">Host Name As Link</Link>
-                  </div>
-                  <div className="flex">
-                    <p className="w-1/2">Description:</p>
-                    <p className="w-1/2">Location:</p>
-                  </div>
-                  <div className="flex">
-                    <p className="w-1/2">Date:</p>
-                    <p className="w-1/2">Time:</p>
-                  </div>
-
-                  {/* button depending on attendence to join/leave/cancel event*/}
-                </div>
-              </div>
-              {/* good deed end */}
+              {/* end of for each event */}
             </div>
-            <div className="sm:flex sm:px-6 sm:py-5"></div>
-            {/* </dl> */}
+            {/* good deeds */}
+            <div className="w-1/2 m-2 flex-col">
+              <h2>Good Deeds (upcoming or completed)</h2>
+              {userGoodDeeds &&
+                userGoodDeeds.map((goodDeed) => (
+                  <div className="flex-col p-3 mt-2 antialiased bg-white rounded-lg shadow-lg  relative">
+                    <div className="flex-row">
+                      <div className="pb-1 text-2xl text-amber-500">
+                        <span>{goodDeed.title}</span>
+                      </div>
+                      <div className="absolute right-0 top-0 m-1">
+                        Kindly Points: 10
+                      </div>
+                    </div>
+                    <div className="pb-4 cursor-pointer text-normal hover:text-cyan-700 text-cyan-900">
+                      <Link to="">
+                        {goodDeed.host.firstName} {goodDeed.host.lastName}
+                      </Link>
+                    </div>
+                    <div className="flex">
+                      <p className="w-2/3">
+                        <b>Description: </b> {goodDeed.deedText}
+                      </p>
+                      <p className="w-1/3">
+                        <b>Location: </b> {goodDeed.location}
+                      </p>
+                    </div>
+                    <div className="flex">
+                      <p className="w-2/3">
+                        <b>Date: </b>
+                        {goodDeed.date}
+                      </p>
+                      <p className="w-1/3">
+                        <b>Time: </b>
+                        {goodDeed.time}
+                      </p>
+                    </div>
+
+                    {/* button depending on attendence to join/leave/cancel event*/}
+                  </div>
+                ))}
+            </div>
+            {/* good deed end */}
           </div>
+          <div className="sm:flex sm:px-6 sm:py-5"></div>
+          {/* </dl> */}
         </div>
       </div>
     </div>
