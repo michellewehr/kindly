fd2456892915bd52af60f30449c7e885588a11ba
Michelle Wehr <michelle_wehr@icloud.com>
2022-01-05 14:28:29 -0500

    good deed add modal works

--- client/src/components/GoodDeedList/index.js
+++ client/src/components/GoodDeedList/index.js
@@ -1,24 +1,36 @@
 import GoodDeed from "../GoodDeed";
 import { useEffect, useState } from "react";
-import { QUERY_GOOD_DEEDS } from '../../utils/queries'
+import { QUERY_GOOD_DEEDS } from "../../utils/queries";
 import { useQuery } from "@apollo/client";
-import Auth from '../../utils/auth';
+import Auth from "../../utils/auth";
 import GoodDeedModal from "../GoodDeedModal";
-import Loading from '../Loading';
+import Loading from "../Loading";
+import SuccessModal from "../SuccessModal";
 
-export default function GoodDeedList({me}) {
-  const { loading, data } = useQuery(QUERY_GOOD_DEEDS)
+export default function GoodDeedList({ me }) {
+  const { loading, data } = useQuery(QUERY_GOOD_DEEDS);
   const [goodDeedModalOpen, setGoodDeedModalOpen] = useState(false);
+  const [showSuccess, setShowSuccess] = useState(false);
   const goodDeedData = data?.goodDeeds || [];
   const userMeData = me || {};
-  console.log(goodDeedData, 'list good deed data')
+  console.log(goodDeedData, "list good deed data");
   // console.log(goodDeedData)
   if (loading) return <Loading />;
 
+  function onClose() {
+    setGoodDeedModalOpen(false);
+    setShowSuccess(true);
+  }
 
   return (
     <div>
-      {goodDeedModalOpen && <GoodDeedModal onClose={() => { setGoodDeedModalOpen(false) }} />}
+      {showSuccess && (
+        <SuccessModal
+          message={"Good deed successfully added!"}
+          closeSuccess={() => setShowSuccess(false)}
+        />
+      )}
+      {goodDeedModalOpen && <GoodDeedModal onClose={onClose} />}
 
       {loading ? (
         <div>Loading...</div>
@@ -26,7 +38,12 @@ export default function GoodDeedList({me}) {
         <div>
           {Auth.loggedIn() && (
             <div className="relative h-16 text-sm text-amber-500">
-              <button onClick={() => { setGoodDeedModalOpen(true) }} className="absolute right-0 h-16 px-4 py-2 mt-1 mr-2 font-bold text-white rounded bg-cyan-700 hover:bg-orange-300">
+              <button
+                onClick={() => {
+                  setGoodDeedModalOpen(true);
+                }}
+                className="absolute right-0 h-16 px-4 py-2 mt-1 mr-2 font-bold text-white rounded bg-cyan-700 hover:bg-orange-300"
+              >
                 Create Good Deed
               </button>
             </div>
@@ -36,16 +53,16 @@ export default function GoodDeedList({me}) {
               key={goodDeed.title}
               goodDeedData={goodDeed}
               me={userMeData}
-            // host={goodDeed.host}
-            // title={goodDeed.title}
-            // location={goodDeed.location}
-            // comments={goodDeed.comments}
-            // deedText={goodDeed.deedText}
-            // helper={goodDeed.helper}
+              // host={goodDeed.host}
+              // title={goodDeed.title}
+              // location={goodDeed.location}
+              // comments={goodDeed.comments}
+              // deedText={goodDeed.deedText}
+              // helper={goodDeed.helper}
             />
           ))}
         </div>
       )}
     </div>
-  )
+  );
 }
