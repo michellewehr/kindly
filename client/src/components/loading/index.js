06bf49e81ecfd9f00580ec162adbbac800d16fa8
Brent Gaines <84875271+brentocracy@users.noreply.github.com>
2022-01-06 15:16:55 -0500

    Resolve casing issue with build
    Loading component not cased on repo

--- client/src/components/loading/index.js
+++ client/src/components/loading/index.js
@@ -1,9 +1,9 @@
 export default function Loading() {
-  return (
-    <section>
-      <div className="radius-50 absolute bg-stone-600 w-full h-full right-0 left-0 opacity-80 grid place-items-center">
-        <h2 className="text-white text-xl animate-bounce">Loading...</h2>
-      </div>
-    </section>
-  );
+   return (
+      <section>
+         <div className="absolute left-0 right-0 grid w-full h-full radius-50 bg-stone-600 opacity-80 place-items-center">
+            <h2 className="text-xl text-white animate-bounce">Loading...</h2>
+         </div>
+      </section>
+   );
 };
