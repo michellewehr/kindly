b7effd87c14e8b70bb72ab5c656a7d911c51c8a8
Brent Gaines <84875271+brentocracy@users.noreply.github.com>
2022-01-06 15:18:32 -0500

    Remove loading component to re-case

--- client/src/components/loading/index.js
+++
@@ -1,9 +0,0 @@
-export default function Loading() {
-   return (
-      <section>
-         <div className="absolute left-0 right-0 grid w-full h-full radius-50 bg-stone-600 opacity-80 place-items-center">
-            <h2 className="text-xl text-white animate-bounce">Loading...</h2>
-         </div>
-      </section>
-   );
-};
