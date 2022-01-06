2c38d205872322dcce75b955f1bead119843f1dc
Brent Gaines <84875271+brentocracy@users.noreply.github.com>
2022-01-06 16:51:19 -0500

    Rebuild

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:3001/kindly',
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/kindly',
   // 'mongodb://127.0.0.1/kindly',
   {
     useNewUrlParser: true,
