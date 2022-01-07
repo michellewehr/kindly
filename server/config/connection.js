6d428513a9de8266993a0b0a8f63779da99ae55f
Brent Gaines <84875271+brentocracy@users.noreply.github.com>
2022-01-07 17:27:24 -0500

    Test deploy with dev

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI ||
-  //  'mongodb://localhost:3001/kindly',
-  'mongodb://127.0.0.1/kindly',
+  process.env.MONGODB_URI || 'mongodb://localhost:3001/kindly',
+  // 'mongodb://127.0.0.1/kindly',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
